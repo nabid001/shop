9264e2c0b566473d06479db1da9af98438b67813
Nabid <nabid.explore@gmail.com>
2025-09-29 22:49:02 +0600

    chan(shop): change home page UI

--- apps/shop/app/page.tsx
+++ apps/shop/app/page.tsx
@@ -9,7 +9,7 @@ const Home = async () => {
   return (
     <div className="flex items-center justify-center h-screen flex-col gap-10">
       <h1 className="text-4xl font-bold">Hello World</h1>
-      <h3 className="font-bold text-2xl text-sky-100">{products[0].name}</h3>
+      <h3 className="font-bold text-2xl text-sky-300">{products[0].name}</h3>
     </div>
   );
 };
